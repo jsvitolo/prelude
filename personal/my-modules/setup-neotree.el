aae42c132d9b53a48b280613d3ed348ed3c843b4
Jones Vitolo <jsvitolo@gmail.com>
2016-03-24 09:44:07 -0300

    change neotree package

--- personal/my-modules/setup-neotree.el
+++ personal/my-modules/setup-neotree.el
@@ -1,4 +1,6 @@
-(add-to-list 'load-path "~/.emacs.d/personal/preload/neotree/")
+;;git clone https://github.com/jaypei/emacs-neotree.git neotree
+
+(add-to-list 'load-path "~/.emacs.d/personal/packages/neotree/")
 (require 'neotree)
 
 (global-set-key [f8] 'neotree-toggle)
